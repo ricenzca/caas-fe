58b06a968156a20fa0ab32820c22e56d6889832b
ricenzca <ricenzca@hotmail.com>
2026-01-23 17:22:39 +0800

    return error msg and status for api routes

--- src/app/flights/[id]/route.ts
+++ src/app/flights/[id]/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server"
 export async function GET(_req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
     const res = await getFlightPlanDetails(id)
+
     if (res instanceof Error) {
         return NextResponse.json({ error: res.message }, { status: 500 })
     }
--- src/app/flights/route.ts
+++ src/app/flights/route.ts
@@ -4,5 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     const queryParams = req.nextUrl.searchParams;
     const callSign = queryParams.get('callSign');
-    return NextResponse.json(await getFlightPlans(callSign))
+    const res = await getFlightPlans(callSign);
+    if (res instanceof Error) {
+        return NextResponse.json({ error: res.message }, { status: 500 })
+    }
+    return NextResponse.json(res)
 }
