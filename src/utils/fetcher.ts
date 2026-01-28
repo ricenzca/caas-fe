79ec868741d46de42bd88049d3bd55c00f9dea10
ricenzca <ricenzca@hotmail.com>
2026-01-28 12:14:01 +0800

    fetcher add try catch

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,13 +1,19 @@
 type FetcherArgs = [string | URL | Request]
 
 const fetcher = async (...args: FetcherArgs) => {
-    const res = await fetch(...args)
-    if (!res.ok) {
-        const error = new Error('An error occurred while fetching the data.')
-        error.cause = await res.json()
-        throw error
+    try {
+        const res = await fetch(...args)
+        if (!res.ok) {
+            const error = new Error('An error occurred while fetching the data.')
+            error.cause = await res.json()
+            throw error
+        }
+        return await res.json()
+    } catch (error) {
+        const errMsg = 'Error encountered while getting flight plan details'
+        console.error(errMsg, error);
+        return new Error(errMsg);
     }
-    return await res.json()
 }
 
 export default fetcher
